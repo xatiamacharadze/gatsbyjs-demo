1ccd64c00dfe44f3f453e6d836369a432b6be5a4
Khatia Macharadze <kmach12@freeuni.edu.ge>
2020-09-10 21:59:39 +0400

    Add Tweenmax animation

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,30 +1,18 @@
 /* eslint-disable jsx-a11y/anchor-has-content, jsx-a11y/anchor-is-valid*/
 
-import React, { useState } from "react";
+import React, { Component, useRef } from "react";
 import PropTypes from "prop-types";
-import { Link } from "gatsby";
+// import { Link } from "gatsby";
 import { StaticQuery, graphql } from "gatsby";
-import { HelmetDatoCms } from "gatsby-source-datocms";
+// import { HelmetDatoCms } from "gatsby-source-datocms";
 import { TweenMax } from "gsap";
 
 import "../styles/index.sass";
 
-function parallaxIt(e, target, movement) {
-  let $this = document.getElementById("container");
-  let relX = e.pageX - $this.offset().left;
-  let relY = e.pageY - $this.offset().top;
+const TemplateWrapper1 = ({ children }) => {
 
-  // console.log(e.pageX, e.pageY, $this.offset().left, $this.offset().top);
-
-  TweenMax.to(target, 1, {
-    x: (relX - $this.width() / 2) / $this.width() * movement,
-    y: (relY - $this.height() / 2) / $this.height() * movement
-  });
-}
-
-const TemplateWrapper = ({ children }) => {
-
-  const [showMenu, setShowMenu] = useState(false);
+  // const [showMenu, setShowMenu] = useState(false);
+  const [container, nameContainer, mtContainer, mainContainer] = useRef();
 
   return (
     <StaticQuery
@@ -62,11 +50,11 @@ const TemplateWrapper = ({ children }) => {
 
       render={data => (
 
-        <div id='mainContainer' className="main-container">
+        <div ref={mainContainer} className="main-container">
           <header>
             <nav className="header-navigation flex-center">
               <div className="nav-left">
-                <ul className="header-nav flex-center helvetica-medium">
+                <ul className="header-nav flex-center public-sans-thin bolder fc-black">
                   <li className="nav-item">Main</li>
                   <li className="nav-item">About me</li>
                   <li className="nav-item">Procedures</li>
@@ -77,43 +65,43 @@ const TemplateWrapper = ({ children }) => {
                 <span className="emberly-black fs-40 fc-black">MT</span>
               </div>
               <div className="nav-right">
-                <ul className="flex-center helvetica-medium">
+                <ul className="flex-center public-sans-thin bolder fc-black">
                   <li className="nav-item">FAQ</li>
                   <li className="nav-item bg-white">Contact</li>
                 </ul>
               </div>
             </nav>
           </header>
-          <section id="container" className="section-container flex-center">
-            <div id='nameContainer' className="name-container emberly-black-narrow">
+          <section ref={container} className="section-container flex-center">
+            <div ref={nameContainer} className="name-container emberly-black-narrow">
               <span className="name">mariam</span>
               <span className="surname">topuria</span>
             </div>
-            <div id='mtContainer' className="mt-container">
-              <img src={"/mariam_topuria.png"}/>
+            <div ref={mtContainer} className="mt-container">
+              <img src={"/mariam_topuria.png"} alt=""/>
             </div>
           </section>
           <footer className="flex-center">
             <div className="footer-section socials">
               <ul className="flex-center">
-                <li className="footer-item flex-center-vertically">
-                  <span className="helvetica-bold">Instagram</span>
+                <li className="footer-item flex-center-vertically fc-black">
+                  <span className="public-sans-thin bolder">Instagram</span>
                   <span className="public-sans-light">@skincare</span>
                 </li>
-                <li className="footer-item flex-center-vertically">
-                  <span className="helvetica-bold">Facebook</span>
+                <li className="footer-item flex-center-vertically fc-black">
+                  <span className="public-sans-thin bolder">Facebook</span>
                   <span className="public-sans-light">@skincaregeorgia</span>
                 </li>
               </ul>
             </div>
             <div className="footer-section contact">
               <ul className="flex-center">
-                <li className="footer-item flex-center-vertically">
-                  <span className="helvetica-bold">Address</span>
+                <li className="footer-item flex-center-vertically fc-black">
+                  <span className="public-sans-thin bolder">Address</span>
                   <span className="public-sans-light">Lake Adolphus</span>
                 </li>
-                <li className="footer-item flex-center-vertically">
-                  <span className="helvetica-bold">Mobile</span>
+                <li className="footer-item flex-center-vertically fc-black">
+                  <span className="public-sans-thin bolder">Mobile</span>
                   <span className="public-sans-light">599 98 98 98</span>
                 </li>
               </ul>
@@ -127,9 +115,114 @@ const TemplateWrapper = ({ children }) => {
   );
 };
 
-TemplateWrapper.propTypes = {
+TemplateWrapper1.propTypes = {
   children: PropTypes.object
 };
 
-export default TemplateWrapper;
+// export default TemplateWrapper1;
 /* eslint-enable jsx-a11y/anchor-has-content, jsx-a11y/anchor-is-valid*/
+// const [container, nameContainer, mtContainer, mainContainer] = useRef();
+
+
+export default class TemplateWrapper extends Component {
+
+  container = null;
+  nameContainer = null;
+  mtContainer = null;
+  mainContainer = null;
+
+  constructor(props) {
+    super(props);
+    this.container = React.createRef();
+    this.nameContainer = React.createRef();
+    this.mtContainer = React.createRef();
+    this.mainContainer = React.createRef();
+
+  }
+
+  parallaxIt(e, target, movement) {
+    let $this = document.getElementById('container').getBoundingClientRect();
+
+    let relX = e.pageX - $this.left;
+    let relY = e.pageY - $this.top;
+
+
+    TweenMax.to(target, 1, {
+      x: (relX - $this.width / 2) / $this.width * movement,
+      y: (relY - $this.height / 2) / $this.height * movement
+    });
+  }
+
+
+  componentDidMount() {
+    document.addEventListener('mousemove', (e) => {
+      this.parallaxIt(e, '#nameContainer', -40);
+      this.parallaxIt(e, '#mtContainer', -20);
+    });
+
+  }
+
+  render() {
+
+    return <div ref={this.mainContainer} id="mainContainer" className="main-container">
+      <header>
+        <nav className="header-navigation flex-center">
+          <div className="nav-left">
+            <ul className="header-nav flex-center public-sans-thin bolder fc-black">
+              <li className="nav-item">Main</li>
+              <li className="nav-item">About me</li>
+              <li className="nav-item">Procedures</li>
+              <li className="nav-item">Works</li>
+            </ul>
+          </div>
+          <div className="mt-logo bg-white flex-center">
+            <span className="emberly-black fs-40 fc-black">MT</span>
+          </div>
+          <div className="nav-right">
+            <ul className="flex-center public-sans-thin bolder fc-black">
+              <li className="nav-item">FAQ</li>
+              <li className="nav-item bg-white">Contact</li>
+            </ul>
+          </div>
+        </nav>
+      </header>
+      <section ref={this.container} id="container" className="section-container flex-center">
+        <div ref={this.nameContainer} id="nameContainer" className="name-container emberly-black-narrow">
+          <span className="name">mariam</span>
+          <span className="surname">topuria</span>
+        </div>
+        <div ref={this.mtContainer} id="mtContainer" className="mt-container">
+          <img src={"/mariam_topuria.png"} alt=""/>
+        </div>
+      </section>
+      <footer className="flex-center">
+        <div className="footer-section socials">
+          <ul className="flex-center">
+            <li className="footer-item flex-center-vertically fc-black">
+              <span className="public-sans-thin bolder">Instagram</span>
+              <span className="public-sans-light">@skincare</span>
+            </li>
+            <li className="footer-item flex-center-vertically fc-black">
+              <span className="public-sans-thin bolder">Facebook</span>
+              <span className="public-sans-light">@skincaregeorgia</span>
+            </li>
+          </ul>
+        </div>
+        <div className="footer-section contact">
+          <ul className="flex-center">
+            <li className="footer-item flex-center-vertically fc-black">
+              <span className="public-sans-thin bolder">Address</span>
+              <span className="public-sans-light">Lake Adolphus</span>
+            </li>
+            <li className="footer-item flex-center-vertically fc-black">
+              <span className="public-sans-thin bolder">Mobile</span>
+              <span className="public-sans-light">599 98 98 98</span>
+            </li>
+          </ul>
+        </div>
+      </footer>
+    </div>;
+
+    // )}
+  }
+}
